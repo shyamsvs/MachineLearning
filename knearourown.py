67bfdd7aca20b39c741e4082d3741502b54536b2
shyamsvs <34570424+shyamsvs@users.noreply.github.com>
2018-05-28 16:21:42 +0530

    Add files via upload

--- knearourown.py
+++ knearourown.py
@@ -14,9 +14,34 @@ from collections import Counter
 style.use('fivethirtyeight')
 
 dataset = {'k' : [[1,2], [2,3], [3,1]], 'r': [[6,5], [7,7], [8,6]] }
-new_feature = [5,7]
+new_feature = [1,4]
+
+#[[plt.scatter(ii[0], ii[1], s=100, color= i) for ii in dataset[i]] for i in dataset]
+
+#plt.scatter(new_feature[0], new_feature[1])
+#plt.show()
+
+
+def k_near_neighbors(data, predict, k=3):
+    if(len(data) >= k):
+        warnings.warn("K IS LESS THAN YOU ENTERED")
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_distance, group])
+    votes = [i[1] for i in sorted(distances) [:k]]
+    print( Counter(votes).most_common(1)[0][0])
+    vote_results = Counter(votes).most_common(1)[0][0]
+    return vote_results
+
+
+result =  k_near_neighbors(dataset, new_feature, k= 3)
+print(result)     
+
 
 [[plt.scatter(ii[0], ii[1], s=100, color= i) for ii in dataset[i]] for i in dataset]
 
-plt.scatter(new_feature[0], new_feature[1])
+plt.scatter(new_feature[0], new_feature[1], color = result)
 plt.show()
+
